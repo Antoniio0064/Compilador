aebec9b4da2446c88dbbb94c38b80e0631056800
Antoniio0064 <54119916+Antoniio0064@users.noreply.github.com>
2020-09-22 00:39:13 -0500

    Se elimino el número de caracteres como una propiedad de la clase linea y se organizo el código del Form

--- Compilador/Form1.cs
+++ Compilador/Form1.cs
@@ -1,27 +1,22 @@
-﻿using System;
+﻿using Compilador.Transversal;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.IO;
-using Compilador.Transversal;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace Compilador
 {
     public partial class Form1 : Form
     {
         public static List<Linea> lineas = new List<Linea>();
-        private int idEjecucion = 0;
+        private int _idEjecucion = 0;
 
         private const string ARCHIVO_NO_ENCONTRADO_INFORMACION = "Archivo Inexistente en la ruta especificada";
         private const string ARCHIVO_NO_ENCONTRADO_TITULO = "Archivo no encontrado";
         private const string CONSOLA_VACIA_INFORMACION = "Por favor ingresa texto en la consola para poder llevar a cabo el análisis";
         private const string CONSOLA_VACIA_TITULO = "Consola vacía";
-
 
         public Form1()
         {
@@ -94,40 +89,37 @@ namespace Compilador
         {
             if (txtFuente.Text != string.Empty)
                 PasarDeFuenteADestino();
-            
             else
                 MessageBox.Show(CONSOLA_VACIA_INFORMACION, CONSOLA_VACIA_TITULO, MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
 
         private void PasarDeFuenteADestino()
         {
-            idEjecucion++;
+            _idEjecucion++;
 
             CargarEnCache();
 
             LimpiarConsolas();
-            
+
             EscribirLineaDesdeCache();
 
             txtDestino.Enabled = true;
-            lstEjecuciones.Items.Add($"Ejecución: {idEjecucion}");
+            lstEjecuciones.Items.Add($"Ejecución: {_idEjecucion}");
         }
 
         private void CargarEnCache()
         {
             for (int i = 0; i < txtFuente.Lines.Count(); i++)
             {
-                int numeroCaracteres = txtFuente.Lines[i].Length;
                 string contenido = txtFuente.Lines[i];
                 int numeroLinea = i + 1;
-
-                lineas.Add(new Linea(idEjecucion, numeroCaracteres, contenido, numeroLinea));
+                lineas.Add(new Linea(_idEjecucion, contenido, numeroLinea));
             }
         }
 
         private void EscribirLineaDesdeCache()
         {
-            var lineasEjecucionActual = lineas.Where(l => (l.IdEjecucion == idEjecucion));
+            var lineasEjecucionActual = lineas.Where(l => (l.IdEjecucion == _idEjecucion));
 
             foreach (Linea linea in lineasEjecucionActual)
             {
@@ -135,12 +127,10 @@ namespace Compilador
 
                 txtDestino.Text += $"{linea.NumeroLinea}->{lineaSinEspacios} {Environment.NewLine}";
             }
-
         }
 
         private void lstEjecuciones_SelectedIndexChanged(object sender, EventArgs e)
         {
-
             LimpiarConsolas();
 
             MostrarEnConsolaEjecuionSeleccionada();
@@ -148,7 +138,6 @@ namespace Compilador
 
         private void MostrarEnConsolaEjecuionSeleccionada()
         {
-
             var lineasEnEjecucion = lineas.Where(linea => (linea.IdEjecucion - 1) == lstEjecuciones.SelectedIndex);
 
             foreach (Linea linea in lineasEnEjecucion)
--- Compilador/Transversal/Linea.cs
+++ Compilador/Transversal/Linea.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Compilador.Transversal
+﻿namespace Compilador.Transversal
 {
     public class Linea
     {
         public int IdEjecucion { get; private set; }
-        public int NumeroCaracteres { get; private set; }
         public string Contenido { get; private set; }
         public int NumeroLinea { get; private set; }
 
-        public Linea(int idEjecucion, int numeroCaracteres, string contenido, int numeroLinea)
+        public Linea(int idEjecucion, string contenido, int numeroLinea)
         {
             IdEjecucion = idEjecucion;
-            NumeroCaracteres = numeroCaracteres;
             Contenido = contenido;
             NumeroLinea = numeroLinea;
         }
