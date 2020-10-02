114e9b67d2095b75abd2c85b7d8c343965b539e5
Antonio Alzate <antonioalzate7@gmail.com>
2020-10-02 00:24:36 -0500

    Componente de almacenamiento cache y validaciones correspondientes en linea

---
+++ Compilador/Transversal/Cache.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilador.Transversal
+{
+    public class Cache
+    {
+        private static List<Linea> Lineas = new List<Linea>();
+
+        public static void Limpiar()
+        {
+            Lineas.Clear();
+        }
+
+        public static void Poblar(List<Linea> lineas)
+        {
+            Limpiar();
+
+            if(lineas != null && lineas.Count > 0)
+            {
+                Cache.Lineas = lineas;
+            }
+        }
+
+        public static void Agregar(string contenido)
+        {
+            if(contenido != null)
+            {
+                int numeroLinea = Lineas.Count + 1;
+
+                Lineas.Add(Linea.Crear(numeroLinea, contenido));
+            }
+        }
+
+        public static Linea ObtenerLinea(int numeroLinea)
+        {
+            Linea lineaRetorno;
+
+            if (ExisteLinea(numeroLinea))
+            {
+                lineaRetorno = Lineas[numeroLinea - 1];
+            }
+            else
+            {
+                lineaRetorno = Linea.Crear(Lineas.Count + 1, "@EOF@");
+            }
+
+            return lineaRetorno;
+        }
+
+        public static bool ExisteLinea(int numeroLinea)
+        {
+            return ((numeroLinea > 0) && (numeroLinea <= Lineas.Count));
+        }
+
+        public static List<Linea> ObtenerLineas()
+        {
+            return Lineas;
+        }
+    }
+}
--- Compilador/Transversal/Linea.cs
+++ Compilador/Transversal/Linea.cs
@@ -2,13 +2,37 @@
 {
     public class Linea
     {
-        public string Contenido { get; private set; }
-        public int NumeroLinea { get; private set; }
+        private int numero;
+        private string contenido;
 
-        public Linea(string contenido, int numeroLinea)
+        public int Numero
         {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        public string Contenido
+        {
+            get { return contenido; }
+            set 
+            {
+                if (value == null)
+                    contenido = "";
+                else
+                    contenido = value;
+
+            }
+        }
+
+        private Linea(int numero, string contenido)
+        {
+            Numero = numero;
             Contenido = contenido;
-            NumeroLinea = numeroLinea;
+        }
+
+        public static Linea Crear(int numero, string contenido)
+        {
+            return new Linea(numero, contenido);
         }
     }
 }
